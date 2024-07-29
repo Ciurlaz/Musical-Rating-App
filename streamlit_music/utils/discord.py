5e70d0ad573aef74951fd5dd74774ca6de3a60eb
MeeRQ1 <noblemba5@gmail.com>
2024-07-29 14:27:43 -0500

    Update discord.py

--- streamlit_music/utils/discord.py
+++ streamlit_music/utils/discord.py
@@ -1,1 +1,12 @@
+# utils/discord.py
+import requests
 
+WEBHOOK_URL = 'YOUR_DISCORD_WEBHOOK_URL'
+
+def post_to_discord(message):
+    data = {
+        "content": message
+    }
+    response = requests.post(WEBHOOK_URL, json=data)
+    if response.status_code != 204:
+        raise Exception(f"Error posting to Discord: {response.status_code}, {response.text}")
